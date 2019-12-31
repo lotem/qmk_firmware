b2c3f7b746cc0660dd8f565d3c8075791cea2c63
Chen Gong <chen.sst@gmail.com>
2019-12-31 09:23:20 +0800

    (amj40): remap symbol keys in raise/lower layers; add mouse keys and caps lock

--- keyboards/amj40/keymaps/kbcon/keymap.c
+++ keyboards/amj40/keymaps/kbcon/keymap.c
@@ -17,7 +17,7 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-        /* Default Layer
+    /* Default Layer
      * ,-----------------------------------------------------------.
      * | Esc|  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS |
      * |-----------------------------------------------------------|
@@ -35,33 +35,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_SPC),LT(_RAISE, KC_SPC),LT(_ADJUST, KC_LGUI), KC_RALT, KC_RCTL \
         ),
 
+    /* Lower Layer
+     * ,-----------------------------------------------------------.
+     * |  ~ |  ! |  @ |  # |  $ |  % |  ^ |  & |  * |  ( |  ) | BS |
+     * |-----------------------------------------------------------|
+     * |     |  _ |  + |  ( |  ) |  : |Home|PgDn|PgUp|End |  Pipe  |
+     * |-----------------------------------------------------------|
+     * |        |  ~ |  " |  { |  } |Pipe|  _ |  + | MS2 |MSU |MS3 |
+     * |-----------------------------------------------------------|
+     * |      |     |     |           |    MS Btn 1 |MSL| MSD| MSR |
+     * `-----------------------------------------------------------'
+     */
     [_LOWER] = LAYOUT( \
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-        _______, KC_UNDS, KC_PLUS, KC_COLN, KC_DQT,  KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-        _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, S(KC_NUHS),S(KC_NUBS),BL_TOGG, BL_INC, BL_DEC, \
-        _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
+        _______, KC_UNDS, KC_PLUS, KC_LPRN, KC_RPRN, KC_COLN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PIPE, \
+        _______, KC_TILD, KC_DQT,  KC_LCBR, KC_RCBR, KC_PIPE, KC_UNDS, KC_PLUS, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN3, \
+        _______, _______, _______, _______, KC_MS_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT \
         ),
 
+    /* Raise Layer
+     * ,-----------------------------------------------------------.
+     * |  ` |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 | BS |
+     * |-----------------------------------------------------------|
+     * |     |  - |  = |  ( |  ) |  ; |Left| Dn | Up |Rght|  \     |
+     * |-----------------------------------------------------------|
+     * |        |  ` |  ' |  [ |  ] |  \ |  - |  = |     |    |Del |
+     * |-----------------------------------------------------------|
+     * |      |     |     |           |             |   |    |     |
+     * `-----------------------------------------------------------'
+     */
     [_RAISE] = LAYOUT( \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-        _______, KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-        _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NUHS, KC_NUBS, _______, _______, KC_DEL, \
-        _______, KC_TRNS, _______, KC_TRNS, KC_TRNS, _______, _______, RGB_TOG \
+        _______, KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_SCLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS, \
+        _______, KC_GRV,  KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, KC_DEL, \
+        _______, _______, _______, _______, _______, _______, _______, _______ \
         ),
 
     [_ADJUST] = LAYOUT( \
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_DEL, \
-        _______, KC_F11,  KC_F12,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______,  _______,  \
-        _______, _______, _______, RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD, \
+        KC_CAPS, KC_F11,  KC_F12,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, BL_TOGG, BL_INC,  BL_DEC, _______,  \
+        _______, NK_ON,   NK_OFF,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD, \
         KC_SYSTEM_SLEEP, _______, _______, _______, _______, _______, _______, _______ \
         ),
 
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
- 
+
   switch (keycode) {
-    
+
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
