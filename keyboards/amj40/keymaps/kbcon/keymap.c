b3cc66847f787c88ccb6a41e0eead519865faa8b
Chen Gong <chen.sst@gmail.com>
2020-08-28 12:26:51 +0800

    (amj40): swap lower/raise layers

--- keyboards/amj40/keymaps/kbcon/keymap.c
+++ keyboards/amj40/keymaps/kbcon/keymap.c
@@ -39,24 +39,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Lower Layer
      * ,-----------------------------------------------------------.
-     * |  ~ |  ! |  @ |  # |  $ |  % |  ^ |  & |  * |  ( |  ) | BS |
-     * |-----------------------------------------------------------|
-     * |     |  _ |  + |  ( |  ) |  : |Home|PgDn|PgUp|End |  Pipe  |
-     * |-----------------------------------------------------------|
-     * |        |  ~ |  " |  { |  } |Pipe|  _ |  + | MS2 |MSU |MS3 |
-     * |-----------------------------------------------------------|
-     * |      |     |     |           |    MS Btn 1 |MSL| MSD| MSR |
-     * `-----------------------------------------------------------'
-     */
-    [_LOWER] = LAYOUT( \
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-        _______, KC_UNDS, KC_PLUS, KC_LPRN, KC_RPRN, KC_COLN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PIPE, \
-        _______, KC_TILD, KC_DQT,  KC_LCBR, KC_RCBR, KC_PIPE, KC_UNDS, KC_PLUS, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN3, \
-        _______, _______, _______, _______, KC_MS_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT \
-        ),
-
-    /* Raise Layer
-     * ,-----------------------------------------------------------.
      * |  ` |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 | BS |
      * |-----------------------------------------------------------|
      * |     |  - |  = |  ( |  ) |  ; |Left| Dn | Up |Rght|  \     |
@@ -66,11 +48,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |     |     |           |             |   |    |     |
      * `-----------------------------------------------------------'
      */
-    [_RAISE] = LAYOUT( \
+    [_LOWER] = LAYOUT( \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
         _______, KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_SCLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS, \
         _______, KC_GRV,  KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, KC_DEL, \
         _______, _______, _______, _______, _______, _______, _______, _______ \
+        ),
+
+    /* Raise Layer
+     * ,-----------------------------------------------------------.
+     * |  ~ |  ! |  @ |  # |  $ |  % |  ^ |  & |  * |  ( |  ) | BS |
+     * |-----------------------------------------------------------|
+     * |     |  _ |  + |  ( |  ) |  : |Home|PgDn|PgUp|End |  Pipe  |
+     * |-----------------------------------------------------------|
+     * |        |  ~ |  " |  { |  } |Pipe|  _ |  + | MS2 |MSU |MS3 |
+     * |-----------------------------------------------------------|
+     * |      |     |     |           |    MS Btn 1 |MSL| MSD| MSR |
+     * `-----------------------------------------------------------'
+     */
+    [_RAISE] = LAYOUT( \
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+        _______, KC_UNDS, KC_PLUS, KC_LPRN, KC_RPRN, KC_COLN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PIPE, \
+        _______, KC_TILD, KC_DQT,  KC_LCBR, KC_RCBR, KC_PIPE, KC_UNDS, KC_PLUS, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN3, \
+        _______, _______, _______, _______, KC_MS_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT \
         ),
 
     /* Adjust Layer
