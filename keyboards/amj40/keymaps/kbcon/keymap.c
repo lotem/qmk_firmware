075d0f135fdbb8fe4713e3da5d02ceab7a1e5d79
Chen Gong <chen.sst@gmail.com>
2020-08-28 14:41:55 +0800

    (amj40): remove mac layer
    use bootmangic instead. the layer toggle key did not work, it should be in the last layer.

--- keyboards/amj40/keymaps/kbcon/keymap.c
+++ keyboards/amj40/keymaps/kbcon/keymap.c
@@ -8,14 +8,12 @@
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
-#define _MAC 4
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
-  MAC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -27,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |  \ fn  |  Z |  X |  C |  V |  B |  N |  M |  ,  |  . |fn/ |
      * |-----------------------------------------------------------|
-     * | LCtl | LGui| LAlt|   LSht    |     spc     |fn2|RAlt|RCtl |
+     * | LCtl | LAlt| LGui|   LSht    |     spc     |fn2|RAlt|RCtl |
      * `-----------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT( \
         KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,\
         LT(_RAISE, KC_TAB), KC_A, KC_S,  KC_D,   KC_F,   KC_G,   KC_H,   KC_J,    KC_K,    KC_L,   LT(_RAISE, KC_SCLN), \
         LT(_LOWER, KC_BSLS), KC_Z, KC_X, KC_C,   KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, LT(_LOWER, KC_SLSH), \
-        KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_SPC, LT(_ADJUST, KC_LGUI), KC_RALT, KC_RCTL \
+        KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_SPC, LT(_ADJUST, KC_LGUI), KC_RALT, KC_RCTL \
         ),
 
     /* Lower Layer
@@ -81,32 +79,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |        | NK+| NK-|RGB*|    |    |    |    |     |    |    |
      * |-----------------------------------------------------------|
-     * |SYSSLP|     | Mac*|           |             |   |    |     |
+     * |SYSSLP|     |     |           |             |   |    |     |
      * `-----------------------------------------------------------'
      */
     [_ADJUST] = LAYOUT( \
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL, \
         KC_CAPS, KC_F11,  KC_F12,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, BL_TOGG, BL_INC,  BL_DEC,  _______, \
         _______, NK_ON,   NK_OFF,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, \
-        KC_SYSTEM_SLEEP, _______, TG(_MAC), _______, _______, _______, _______, _______ \
-        ),
-
-    /* Mac Layer
-     * ,-----------------------------------------------------------.
-     * |    |    |    |    |    |    |    |    |    |    |    |    |
-     * |-----------------------------------------------------------|
-     * |     |    |    |    |    |    |    |    |    |    |        |
-     * |-----------------------------------------------------------|
-     * |        |    |    |    |    |    |    |    |     |    |    |
-     * |-----------------------------------------------------------|
-     * |      | LAlt| LGui|           |             |   |    |     |
-     * `-----------------------------------------------------------'
-     */
-    [_MAC] = LAYOUT( \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, KC_LALT, KC_LGUI, _______, _______, _______, _______, _______ \
+        KC_SYSTEM_SLEEP, _______,  _______, _______, _______, _______, _______, _______ \
         ),
 
 };
@@ -140,14 +120,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_ADJUST);
       } else {
         layer_off(_ADJUST);
-      }
-      return false;
-      break;
-    case MAC:
-      if (record->event.pressed) {
-        layer_on(_MAC);
-      } else {
-        layer_off(_MAC);
       }
       return false;
       break;
