90d388f1244da83453a366bceff253fa20e5dd8e
Chen Gong <chen.sst@gmail.com>
2020-09-07 00:40:43 +0800

    fix(amj40): Lower+Tab = Caps_Lock

--- keyboards/amj40/keymaps/kbcon/keymap.c
+++ keyboards/amj40/keymaps/kbcon/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------.
      * |  ` |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |Del |
      * |-----------------------------------------------------------|
-     * |     |  \ |  ' |  ( |  ) |  ; |Left| Dn | Up |Rght|  '     |
+     * |Caps |  \ |  ' |  ( |  ) |  ; |Left| Dn | Up |Rght|  '     |
      * |-----------------------------------------------------------|
      * |        |  - |  = |  [ |  ] |  \ |  - |  = |     |    |    |
      * |-----------------------------------------------------------|
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_LOWER] = LAYOUT( \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-        _______, KC_BSLS, KC_QUOT, KC_LPRN, KC_RPRN, KC_SCLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, \
+        KC_CAPS, KC_BSLS, KC_QUOT, KC_LPRN, KC_RPRN, KC_SCLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, \
         _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______ \
         ),
