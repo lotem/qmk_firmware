3fa77a6cde4346e64425c1f63de3d44ca184d28c
Chen Gong <chen.sst@gmail.com>
2020-09-25 00:08:26 +0800

    (ergodox_ez): optimize symbol keys

--- keyboards/ergodox_ez/keymaps/kbcon/keymap.c
+++ keyboards/ergodox_ez/keymaps/kbcon/keymap.c
@@ -21,11 +21,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   -    |   1  |   2  |   3  |   4  |   5  |      |           | NumLk|   6  |   7  |   8  |   9  |   0  |   =    |
+ * |   [    |   1  |   2  |   3  |   4  |   5  |      |           | NumLk|   6  |   7  |   8  |   9  |   0  |   ]    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   [    |   Q  |   W  |   E  |   R  |   T  |   `  |           | Del  |   Y  |   U  |   I  |   O  |   P  |   ]    |
+ * |   =    |   Q  |   W  |   E  |   R  |   T  |   `  |           | Del  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |   \    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |   -    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  Tab |           | BkSp |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -41,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
-  KC_MINS,      KC_1,    KC_2,     KC_3,     KC_4,    KC_5,     KC_NO,
-  KC_LBRC,      KC_Q,    KC_W,     KC_E,     KC_R,    KC_T,     KC_GRV,
-  KC_BSLS,      KC_A,    KC_S,     KC_D,     KC_F,    KC_G,
+  KC_LBRC,      KC_1,    KC_2,     KC_3,     KC_4,    KC_5,     KC_NO,
+  KC_EQL,       KC_Q,    KC_W,     KC_E,     KC_R,    KC_T,     KC_GRV,
+  KC_MINS,      KC_A,    KC_S,     KC_D,     KC_F,    KC_G,
   KC_LSFT,      KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,     KC_TAB,
   MO(MDIA),     MO(SYMB), KC_LEFT, KC_RGHT,  MO(SYMB),
                                                       MO(MDIA), KC_LCTL,
                                                                 KC_LALT,
                                         KC_LCTL, SFT_T(KC_ESC), KC_LGUI,
   // right hand
-  TG(MDIA),     KC_6,    KC_7,     KC_8,     KC_9,    KC_0,     KC_EQL,
-  KC_DEL,       KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,     KC_RBRC,
+  TG(MDIA),     KC_6,    KC_7,     KC_8,     KC_9,    KC_0,     KC_RBRC,
+  KC_DEL,       KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,     KC_BSLS,
                 KC_H,    KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
   KC_BSPC,      KC_N,    KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
                          MO(SYMB), KC_DOWN,  KC_UP,   MO(SYMB), MO(MDIA),
