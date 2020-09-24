81d1db6c584af477c4125363004898eca9f7cdad
Chen Gong <chen.sst@gmail.com>
2020-09-25 00:11:37 +0800

    (ergodox_ez): optimize media keys

--- keyboards/ergodox_ez/keymaps/kbcon/keymap.c
+++ keyboards/ergodox_ez/keymaps/kbcon/keymap.c
@@ -146,13 +146,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |Version |      |      |      |      |      | STENO|           |      |      |      |      |      |      |  EPRM  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Prev  |      |      | MsUp |      |      |      |           |      |      |   7  |   8  |   9  |      |  VolUp |
+ * |        |      |      | MsUp |      |      |      |           |      |      |   7  |   8  |   9  |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Play  |      |MsLeft|MsDown|MsRght|      |------|           |------|      |   4  |   5  |   6  |      |  Mute  |
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |   4  |   5  |   6  |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Next  |      |      |      |      |      |      |           |      |      |   1  |   2  |   3  |      |  VolDn |
+ * |        |      |      |      |      |      |      |           |      |      |   1  |   2  |   3  |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   |      |      | Prev | Next | Play |                                       | Mute | VolDn| VolUp|      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -165,19 +165,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MDIA] = LAYOUT_ergodox(
   // left hand
   VRSN,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(STENO),
-  KC_MPRV, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_MPLY, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-  KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_MPLY,
                                                KC_TRNS, KC_TRNS,
                                                         KC_APP,
                                       KC_BTN1, KC_BTN2, KC_WBAK,
   // right hand
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EPRM,
-  KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_VOLU,
-           KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_MUTE,
-  KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_VOLD,
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,
+           KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,
+                    KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS,
   KC_TRNS, KC_DOT,  KC_0
