f299f52300f821c4db8856dbd1011980ebfd0b83
Chen Gong <chen.sst@gmail.com>
2020-10-04 23:15:30 +0800

    (amj40): conventional default layout with right shift; optimize modifier keys in combo layer

--- keyboards/amj40/keymaps/kbcon/keymap.c
+++ keyboards/amj40/keymaps/kbcon/keymap.c
@@ -27,16 +27,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * | Tab |  A |  S |  D |  F |  G |  H |  J |  K |  L |  Enter |
      * |-----------------------------------------------------------|
-     * | LSft   | ^Z |  X |  C |  V |  B |  N |  M |  ,  |  . | ^/ |
+     * | LSft   |  Z |  X |  C |  V |  B |  N |  M |  ,  |  . | S/ |
      * |-----------------------------------------------------------|
-     * | LCtl | LGui| LAlt|   Lower   |     spc     |Alt|Adju|RCtl |
+     * | LCtl | LGui| LAlt| Lower/spc |  Raise/spc  |Alt| fn |RCtl |
      * `-----------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT( \
-        KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC, \
+        KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
         KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT, \
-        KC_LSFT, LT(_RAISE, KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,   KC_COMM, KC_DOT, LT(_RAISE, KC_SLSH), \
-        KC_LCTL, KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_RALT, MO(_ADJUST), KC_RCTL \
+        KC_LSFT, LT(_RAISE, KC_Z), KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), \
+        KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), KC_RALT, MO(_ADJUST), KC_RCTL \
         ),
 
     /* Homely Layer
@@ -65,39 +65,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |        |    |    |    |    |    |    |    |     |    |    |
      * |-----------------------------------------------------------|
-     * | Lower|     |     |    Ctrl   |     spc     |   |    |Lowr |
+     * | LALt |     |Lower|    Ctrl   |     spc     | Rs|    |RAlt |
      * `-----------------------------------------------------------'
      */
     [_COMBO] = LAYOUT( \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        MO(_LOWER), _______, _______, KC_LCTL, KC_SPC, _______, _______, MO(_LOWER) \
+        KC_LALT, _______, MO(_LOWER), KC_LCTL, KC_SPC, MO(_RAISE), _______, KC_RALT \
         ),
 
     /* Lower Layer
      * ,-----------------------------------------------------------.
      * |  ` |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |Del |
      * |-----------------------------------------------------------|
-     * |Caps |  \ |  ' |  ( |  ) |  ; |Left| Dn | Up |Rght|  '     |
+     * |Raise|  \ |  ' |  ( |  ) |  ; |Left| Dn | Up |Rght|  Enter |
      * |-----------------------------------------------------------|
      * |        |  - |  = |  [ |  ] |  \ |  - |  = |     |    |    |
      * |-----------------------------------------------------------|
-     * |      |     |     |           |     Ent     |   |    |     |
+     * |      |     |     |           |             |   |    |     |
      * `-----------------------------------------------------------'
      */
     [_LOWER] = LAYOUT( \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-        KC_CAPS, KC_BSLS, KC_QUOT, KC_LPRN, KC_RPRN, KC_SCLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, \
+        MO(_RAISE), KC_BSLS, KC_QUOT, KC_LPRN, KC_RPRN, KC_SCLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT, \
         _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, _______, \
-        _______, _______, _______, _______, KC_ENT,  _______, _______, _______ \
+        _______, _______, _______, _______, _______, _______, _______, _______ \
         ),
 
     /* Raise Layer
      * ,-----------------------------------------------------------.
      * |  ~ |  ! |  @ |  # |  $ |  % |  ^ |  & |  * |  ( |  ) |Del |
      * |-----------------------------------------------------------|
-     * |Caps |Pipe|  " |  ( |  ) |  : |Home|PgDn|PgUp|End |  "     |
+     * |Caps |Pipe|  " |  ( |  ) |  : |Home|PgDn|PgUp|End |  Enter |
      * |-----------------------------------------------------------|
      * |        |  _ |  + |  { |  } |Pipe|  _ |  + | MS2 |MSU |MS3 |
      * |-----------------------------------------------------------|
@@ -106,27 +106,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_RAISE] = LAYOUT( \
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-        KC_CAPS, KC_PIPE, KC_DQT,  KC_LPRN, KC_RPRN, KC_COLN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQT, \
+        KC_CAPS, KC_PIPE, KC_DQT,  KC_LPRN, KC_RPRN, KC_COLN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT, \
         _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_UNDS, KC_PLUS, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN3, \
         _______, _______, _______, _______, KC_MS_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT \
         ),
 
     /* Adjust Layer
      * ,-----------------------------------------------------------.
-     * |SLEP| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10| Del|
+     * |SLEP| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10|RSET|
      * |-----------------------------------------------------------|
-     * |Caps | F11| F12| AU+| AU-| AG=|AGSW| BL*| BL+| BL-| Homely*|
+     * |Caps | F11| F12|    |    |    |Home|PgDn|PgUp|End | Homely*|
      * |-----------------------------------------------------------|
-     * |        | NK+| NK-|RGB*|    |    |    |    |     |    |    |
+     * |        |Gui-|Gui+|AGSw|AGNo| NK-| NK+|    |     |    |    |
      * |-----------------------------------------------------------|
-     * |      |     |     |           |             |   |    |Comb*|
+     * |      |     |     |   Combo*  |             |   |    |     |
      * `-----------------------------------------------------------'
      */
     [_ADJUST] = LAYOUT( \
-        KC_SLEP, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL, \
-        KC_CAPS, KC_F11,  KC_F12,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, BL_TOGG, BL_INC,  BL_DEC,  TG(_HOMELY), \
-        _______, NK_ON,   NK_OFF,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, \
-        _______, _______, _______, _______, _______, _______, _______, TG(_COMBO) \
+        KC_SLEP, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET, \
+        KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  TG(_HOMELY), \
+        _______, GUI_OFF, GUI_ON,  AG_SWAP, AG_NORM, NK_OFF,  NK_ON,   _______, _______, _______, _______, \
+        _______, _______, _______, TG(_COMBO), _______, _______, _______, _______ \
         ),
 
 };
