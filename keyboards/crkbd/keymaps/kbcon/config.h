9a314578efce11b1f38785fdfe34f3c7f5cf6dcd
Chen Gong <chen.sst@gmail.com>
2020-11-10 15:14:41 +0800

    (crkbd): force NKRO

--- keyboards/crkbd/keymaps/kbcon/config.h
+++ keyboards/crkbd/keymaps/kbcon/config.h
@@ -28,6 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 #define EE_HANDS
 
+#define FORCE_NKRO
+
 #define SSD1306OLED
 
 #define USE_SERIAL_PD2
