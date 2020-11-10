29e5c0cea4ef3e1e6972c7920da235685d131253
Chen Gong <chen.sst@gmail.com>
2020-11-10 15:16:01 +0800

    (crkbd): move ESC, Enter to thumb cluster; move Ctrl, space to middle; remap thumb keys in Steno layer

--- keyboards/crkbd/keymaps/kbcon/keymap.c
+++ keyboards/crkbd/keymaps/kbcon/keymap.c
@@ -22,6 +22,7 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  STENO,
 };
 
 enum macro_keycodes {
@@ -29,18 +30,17 @@ enum macro_keycodes {
 };
 
 #define ALT_ESC LALT_T(KC_ESC)
-#define SFT_QUO RSFT_T(KC_QUOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      ALT_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,\
+      KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SFT_QUO,\
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER, KC_LCTL,     KC_SPC,   RAISE, KC_RALT \
+                                            LOWER, KC_LCTL, ALT_ESC,     KC_ENT,  KC_SPC,   RAISE \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
    TG(_STENO),   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_MPRV,                      KC_VOLU, KC_BTN1, KC_MS_U, KC_BTN2, KC_ACL0,   RESET,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_MPLY,                      KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1, XXXXXXX,\
+      _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_MPLY,                      KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1, _______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_MNXT,                      KC_VOLD, KC_BTN3, KC_WH_U, KC_WH_D, KC_ACL2, XXXXXXX,\
+      _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_MNXT,                      KC_VOLD, KC_BTN3, KC_WH_U, KC_WH_D, KC_ACL2, _______,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______ \
                                       //`--------------------------'  `--------------------------'
@@ -82,13 +82,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_STENO] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-   TG(_STENO),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,\
+   TG(_STENO),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_0,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_LBRC,\
+      _______,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
+      KC_BSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______,    KC_C,    KC_V,       KC_N,    KC_M, _______ \
+                                             KC_C,    KC_V, _______,    _______,    KC_N,    KC_M \
                                       //`--------------------------'  `--------------------------'
   ),
 };
