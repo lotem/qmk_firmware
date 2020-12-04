f838d29d340f02d3e49e8bb8597d4a9439134ce5
Chen Gong <chen.sst@gmail.com>
2020-12-05 01:31:49 +0800

    (amj40): revise layout, remap left thumb key to Control

--- keyboards/amj40/keymaps/kbcon/keymap.c
+++ keyboards/amj40/keymaps/kbcon/keymap.c
@@ -18,51 +18,51 @@ enum custom_keycodes {
   ADJUST,
 };
 
-#define ALT_ESC LALT_T(KC_ESC)
-#define ALT_DEL RALT_T(KC_DEL)
+#define RAI_ESC LT(_RAISE, KC_ESC)
+#define SFT_SLSH RSFT_T(KC_SLSH)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Default Layer
      * ,-----------------------------------------------------------.
      * | Tab|  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS |
      * |-----------------------------------------------------------|
-     * | Esc |  A |  S |  D |  F |  G |  H |  J |  K |  L |  Enter |
+     * | Esc^|  A |  S |  D |  F |  G |  H |  J |  K |  L |  Enter |
      * |-----------------------------------------------------------|
      * | LSft   |  Z |  X |  C |  V |  B |  N |  M |  ,  |  . | S/ |
      * |-----------------------------------------------------------|
-     * | Ctrl | Gui |Lower|    Ctrl   |     spc     | Rs| Del|  fn |
+     * | Lower| Gui | Alt |    Ctrl   |     spc     |Alt| fn |Rais |
      * `-----------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT( \
         KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
-        ALT_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT, \
-        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), \
-        KC_LCTL, KC_LGUI, MO(_LOWER), KC_LCTL, KC_SPC, MO(_RAISE), ALT_DEL, MO(_ADJUST) \
+        RAI_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT, \
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  SFT_SLSH, \
+        MO(_LOWER), KC_LGUI, KC_LALT, KC_LCTL, KC_SPC, KC_RALT, MO(_ADJUST), MO(_RAISE) \
         ),
 
     /* Typewriter Layer
      * ,-----------------------------------------------------------.
      * |    |    |    |    |    |    |    |    |    |    |    |    |
      * |-----------------------------------------------------------|
-     * |     |    |    |    |    |    |    |    |    |    |        |
+     * | Esc |    |    |    |    |    |    |    |    |    |        |
      * |-----------------------------------------------------------|
      * |        |    |    |    |    |    |    |    |     |    |    |
      * |-----------------------------------------------------------|
-     * | Ctrl | Gui | Alt | Lower/spc |  Raise/spc  |Alt|    |     |
+     * | Ctrl | Gui | Alt | Lower/spc |  Raise/spc  |Alt| fn |Ctrl |
      * `-----------------------------------------------------------'
      */
     [_TYPEWR] = LAYOUT( \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        KC_LCTL, _______, KC_LALT, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), KC_RALT, KC_DEL, _______ \
+        KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), KC_RALT, MO(_ADJUST), KC_RCTL \
         ),
 
     /* Lower Layer
      * ,-----------------------------------------------------------.
      * |  ~ |  ! |  @ |  # |  $ |  % |  ^ |  & |  * |  ( |  ) |Del |
      * |-----------------------------------------------------------|
-     * |Caps |  ? |  " |  ( |  ) |  : |Left| Dn | Up |Rght|  Enter |
+     * | Caps|  ~ |  " |  ( |  ) |  : |Left| Dn | Up |Rght|        |
      * |-----------------------------------------------------------|
      * |        |  _ |  + |  { |  } |Pipe|  _ |  + | MS2 |MSU |MS3 |
      * |-----------------------------------------------------------|
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_LOWER] = LAYOUT( \
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-        KC_CAPS, KC_QUES, KC_DQT,  KC_LPRN, KC_RPRN, KC_COLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT, \
+        KC_CAPS, KC_TILD, KC_DQT,  KC_LPRN, KC_RPRN, KC_COLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
         _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_UNDS, KC_PLUS, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN3, \
         _______, _______, _______, _______, KC_MS_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT \
                        ),
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------.
      * |  ` |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |Del |
      * |-----------------------------------------------------------|
-     * |Caps |  / |  ' |  ( |  ) |  ; |Home|PgDn|PgUp|End |  Enter |
+     * | Caps|  ` |  ' |  ( |  ) |  ; |Home|PgDn|PgUp|End |        |
      * |-----------------------------------------------------------|
      * |        |  - |  = |  [ |  ] |  \ |  - |  = |     |    |    |
      * |-----------------------------------------------------------|
@@ -89,27 +89,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_RAISE] = LAYOUT( \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-        KC_CAPS, KC_SLSH, KC_QUOT, KC_LPRN, KC_RPRN, KC_SCLN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT, \
+        KC_CAPS, KC_GRV,  KC_QUOT, KC_LPRN, KC_RPRN, KC_SCLN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, \
         _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______ \
                        ),
 
     /* Adjust Layer
      * ,-----------------------------------------------------------.
-     * |SLEP| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10|RSET|
+     * | SLP| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10|RST |
      * |-----------------------------------------------------------|
-     * |Caps | F11| F12|    |    |    |    |    |    |    |        |
+     * | Caps| F11| F12|Prev|Play|Next|Mute|Vol-|Vol+|    |        |
      * |-----------------------------------------------------------|
-     * |        | L0 | L1 | L2 |AGTG| NK-| NK+|    | WhDn|WhUp|    |
+     * |        | BR-| BR+| NK-| NK+|    |    |    | WhDn|WhUp|    |
      * |-----------------------------------------------------------|
-     * |      |     |     |           |             |   |    |     |
+     * |      |     |     |     L0    |     L1      |AGT|    |     |
      * `-----------------------------------------------------------'
      */
     [_ADJUST] = LAYOUT( \
         KC_SLEP, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET, \
-        KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, DF(_QWERTY), DF(_TYPEWR), _______, AG_TOGG, NK_OFF,  NK_ON,   _______, KC_WH_D, KC_WH_U, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______ \
+        KC_CAPS, KC_F11,  KC_F12,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
+        _______, KC_BRID, KC_BRIU, NK_OFF,  NK_ON,   _______, _______, _______, KC_WH_D, KC_WH_U, _______, \
+        _______, _______, _______, DF(_QWERTY), DF(_TYPEWR), AG_TOGG, _______, _______ \
         ),
 
 };
