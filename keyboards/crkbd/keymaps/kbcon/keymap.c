e94e9cf5f474a549d811df40669e0e75b2b8fa55
Chen Gong <chengong@google.com>
2021-01-29 11:59:54 +0800

    (crkbd): remap symbol, num/nav and function keys

--- keyboards/crkbd/keymaps/kbcon/keymap.c
+++ keyboards/crkbd/keymaps/kbcon/keymap.c
@@ -79,11 +79,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      ALT_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,\
+      ALT_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SFT_QUO,\
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SFT_QUO, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER, KC_LCTL,     KC_SPC,   RAISE, KC_RALT \
                                       //`--------------------------'  `--------------------------'
@@ -91,11 +91,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL, \
+      KC_CAPS, KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_PIPE,                       KC_GRV, KC_AMPR, KC_ASTR, KC_QUOT,  KC_DQT,  KC_DEL, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS,  KC_GRV, KC_QUOT, KC_LPRN, KC_RPRN, KC_SCLN,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_COLN,  KC_INS, \
+      KC_LALT, KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN,  KC_TAB,                      KC_BSPC, KC_MINS, KC_PLUS,  KC_EQL, KC_COLN,  KC_INS, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_SLSH, KC_BSLS, KC_LBRC, KC_RBRC, KC_BSLS,                      KC_MINS,  KC_EQL, KC_LABK, KC_RABK, KC_QUOT, _______,\
+      _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BSLS,                      KC_TILD, KC_UNDS, KC_LABK, KC_RABK, KC_QUES, _______, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______ \
                                       //`--------------------------'  `--------------------------'
@@ -103,11 +103,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,\
+      KC_CAPS, KC_SLEP, KC_HOME,   KC_UP,  KC_END, KC_PGUP,                      KC_PLUS,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS, KC_TILD,  KC_DQT, KC_LPRN, KC_RPRN, KC_COLN,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_SCLN,  KC_INS,\
+      KC_LALT,  KC_APP, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                      KC_COLN,    KC_4,    KC_5,    KC_6,  KC_DOT,  KC_INS, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_QUES, KC_PIPE, KC_LCBR, KC_RCBR, KC_PIPE,                      KC_UNDS, KC_PLUS, KC_LABK, KC_RABK,  KC_DQT, _______,\
+      _______, KC_UNDO,  KC_CUT, KC_COPY, KC_PSTE, KC_FIND,                      KC_MINS,    KC_1,    KC_2,    KC_3, KC_SLSH, _______, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______ \
                                       //`--------------------------'  `--------------------------'
@@ -115,13 +115,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       STN_ON,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_WFWD,                      KC_VOLU, KC_BTN1, KC_MS_U, KC_BTN2, KC_MPRV,   RESET, \
+       STN_ON,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_VOLU, KC_WH_D, KC_MS_U, KC_WH_U, KC_MPRV,   RESET, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_APP,                      KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_R, KC_MPLY, KC_RCTL,\
+      KC_LALT,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_R, KC_MPLY, KC_RCTL, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_WBAK,                      KC_VOLD, KC_BTN3, KC_WH_U, KC_WH_D, KC_MNXT, KC_RSFT,\
+      _______,  KC_F11,  KC_F12, KC_PSCR, KC_SLCK, KC_PAUS,                      KC_VOLD, KC_BTN1, KC_BTN3, KC_BTN2, KC_MNXT, KC_RSFT, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______ \
+                                          _______, _______, _______,    KC_BTN1, _______, _______ \
                                       //`--------------------------'  `--------------------------'
   ),
 
